cf5750d282bfdc8fffe387b4b404ced11f59aa73
毕晨曦 <>
2021-06-25 15:42:29 +0800

    fix: comment not null

--- routers/sign.js
+++ routers/sign.js
@@ -6,7 +6,7 @@ router.get('/', async (req,res) => {
     const { account } = req.query
     const result = await sqlAsycn.query({sql: `select * from sign_tab where account = ${account} and DATE_FORMAT(time, '%Y%m' ) = DATE_FORMAT( CURDATE( ) , '%Y%m' )`})
     if (!result.length) {
-        res.send({message: '暂无数据', success: true})
+        res.send([])
         return
     }
 
